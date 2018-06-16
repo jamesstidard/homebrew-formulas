d2995875394f233c9c6b8b49b0ad9c3ea5328a52
James Stidard <james@stidard.com>
2018-06-16 18:54:34 +0100

    v0.0.3

--- mzo.rb
+++ mzo.rb
@@ -1,10 +1,11 @@
+
 class Mzo < Formula
   include Language::Python::Virtualenv
 
-  desc "A command-line interface between you and your Monzo account"
+  desc "A command-line interface between you and your Monzo account."
   homepage "https://mzo.stidard.com"
-  url "https://git@github.com/jamesstidard/Mzo-Cli/archive/v0.0.2.tar.gz"
-  sha256 "86d25cbec7ca1f7411435652e09e965b2edd87412205e83706e5124536c4286d"
+  url "https://github.com/jamesstidard/Mzo-Cli/archive/v0.0.3.tar.gz"
+  sha256 "4aa02bac15a253bcaac686f64910a5d81d556e5e8fe161651d78d5b67e573d31"
 
   depends_on "python"
 
@@ -83,10 +84,7 @@ class Mzo < Formula
     sha256 "5ba766433c30d703f6b2c17eb0b6826c6f898e5f58d89373e235f07764952314"
   end
 
-  # resource "mzo" do
-  #   url "https://files.pythonhosted.org/packages/3f/5d/0c11d6410f8a4c7220378222b64283781775a43186393f2eb888b46510a6/mzo-0.0.2.tar.gz"
-  #   sha256 "bcff578601303cae8921fae06915fee5ec00816591cdbc326970f6fc499c88c7"
-  # end
+
 
   resource "ordered-set" do
     url "https://files.pythonhosted.org/packages/c3/1c/4ddba479a75369b31655e295463bc37e5f84acb4fa7f34548946ff0160e3/ordered-set-2.0.2.tar.gz"
@@ -178,7 +176,9 @@ class Mzo < Formula
     sha256 "c8cbc21bbfa1dd7d5386d48cc814fe3d35b80f60299cdde9279046f399c3b0d8"
   end
 
+
   def install
     virtualenv_install_with_resources
   end
+
 end
